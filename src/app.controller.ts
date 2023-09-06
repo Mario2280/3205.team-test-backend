bf78a7a901ad14a3c78d2281b12f1d141a1daf85
David Gulkevich <david2017ra@gmail.com>
2023-09-06 22:30:40 +0300

    check abort on server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserDto } from './dto/user.dto';
-import { readFileSync } from 'fs';
-import { join } from 'path';
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserDto } from "./dto/user.dto";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 const db: Array<UserDto> = JSON.parse(
-  readFileSync(join(__dirname, '../fake-db.json')).toString(),
+  readFileSync(join(__dirname, "../fake-db.json")).toString(),
 );
 
 @Controller()
 export class AppController {
   @Post()
   async getHello(@Body() { email }: UserDto) {
-    const delay = new Promise<void>((resolve) =>
-      setTimeout(() => resolve(), 5000),
-    );
+    const delay = new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 5000);
+    });
     await delay;
     return db.filter((user) => user.email == email);
   }
