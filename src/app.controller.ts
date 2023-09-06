60d1aae0f300b5341bc52385640960c335e7664c
David Gulkevich <david2017ra@gmail.com>
2023-09-06 19:31:46 +0300

    fix cors nest app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { readFileSync } from 'fs';
 import { join } from 'path';
@@ -9,7 +9,7 @@ const db: Array<UserDto> = JSON.parse(
 
 @Controller()
 export class AppController {
-  @Get()
+  @Post()
   async getHello(@Body() { email }: UserDto) {
     const delay = new Promise<void>((resolve) =>
       setTimeout(() => resolve(), 5000),
